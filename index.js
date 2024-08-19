6b18a79590146f759061792ddd05dd9298c67b15
Bilanox1 <aurabilanox@gmail.com>
2024-08-19 14:58:51 +0100

    updit crose

--- index.js
+++ index.js
@@ -3,30 +3,32 @@ const dotenv = require("dotenv");
 const dbConection = require("./config/connexionDataBase");
 const bookPath = require("./router/BookRouter");
 const cors = require("cors");
-const path = require('path');
+const path = require("path");
 const app = express();
 dotenv.config();
 dbConection();
 
-
-
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use(express.static(path.join(__dirname, 'public')));
-
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
-
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://bilal-ez-zaim.github.io/book-Api1/",
   })
 );
 
+app.use(
+  cors({
+    origin: "https://bilal-ez-zaim.github.io/book-Api1/",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use("/api/book", bookPath);
 
 const PORT = process.env.PORT || 3000;
