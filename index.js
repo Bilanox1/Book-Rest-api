0c64b3ce14b348a6c382ac96b3eb6427c092cf65
Bilanox1 <aurabilanox@gmail.com>
2024-08-19 14:22:36 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -3,12 +3,23 @@ const dotenv = require("dotenv");
 const dbConection = require("./config/connexionDataBase");
 const bookPath = require("./router/BookRouter");
 const cors = require("cors");
-
+const path = require('path');
 const app = express();
 dotenv.config();
 dbConection();
 
+
+
 app.use(express.json());
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 
 app.use(
   cors({
